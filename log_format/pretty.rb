ac28ed50e514ee0a39ab2323c00744e6cf782606
Kevin Fuhst <kfuhst@gmail.com>
2019-08-21 21:33:43 -0600

    add basic log format

---
+++ log_format/pretty.rb
@@ -0,0 +1,11 @@
+module Pretty
+  def self.format(event)
+    values = event.dup.tap do |h|
+      h.delete(:timestamp)
+      h.delete(:activity)
+    end
+    value_string = values.each_pair.collect { |k, v| "#{k}=#{v}" }.join(', ')
+
+    "#{event[:timestamp]} #{event[:activity]} (#{value_string})"
+  end
+end
---
+++ spec/log_format/pretty_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../../log_format/pretty'
+
+describe Pretty do
+  describe '.format' do
+    it 'converts the event map to a formatted string' do
+      event = {
+        timestamp: '2019-08-21T21:06',
+        activity: :foo,
+        bar: 1,
+        baz: 2
+      }
+      expect(Pretty.format(event)).to eql '2019-08-21T21:06 foo (bar=1, baz=2)'
+    end
+  end
+end
