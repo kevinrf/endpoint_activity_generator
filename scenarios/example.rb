a6cc4f8be6059cedeb39a0c30a9ec174717f7e75
Kevin Fuhst <kfuhst@gmail.com>
2019-08-23 11:06:25 -0600

    remove non-portable command from example scenario

--- scenarios/example.rb
+++ scenarios/example.rb
@@ -3,4 +3,3 @@ modify_file 'example.txt', append_data: "Lorem ipsum dolor sit amet\n"
 delete_file 'example.txt'
 transmit_data 'example.com', 80, data: "GET / HTTP/1.1\nHost: example.com\n\n"
 run_process 'echo', 'hello world'
-run_process 'cal'
