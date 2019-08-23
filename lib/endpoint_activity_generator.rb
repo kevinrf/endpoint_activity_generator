f455472b569d827505a2b72b076dc48b1e30ee73
Kevin Fuhst <kfuhst@gmail.com>
2019-08-22 19:48:22 -0600

    add command line interface

--- lib/endpoint_activity_generator.rb
+++ lib/endpoint_activity_generator.rb
@@ -1,3 +1,4 @@
+require 'optparse'
 require_relative 'activity'
 require_relative 'context'
 require_relative 'log'
@@ -5,18 +6,52 @@ require_relative 'log_format'
 require_relative 'scenario_builder'
 require_relative 'scenario_runner'
 
-SCENARIO_PATH = './scenarios/default.rb'
+context = Context.resolve
+
+options = {
+  log_format: :pretty,
+  output: $stdout,
+}
+
+optparse = OptionParser.new do |opts|
+  opts.on('-f', '--file FILE', 'Scenario file to run.') do |f|
+    options[:file] = f
+  end
+
+  opts.on('-l', '--log-format FORMAT',
+          'Log formatter to use (e.g. \'json\').') do |lf|
+    options[:log_format] = lf
+  end
+
+  opts.on('-h', '--help', 'Show this message.') do
+    puts opts
+    exit
+  end
+
+  opts.on('-o', '--output OUTPUT',
+          'Log file destination. If not present, logs are printed to STDOUT.') do |o|
+    options[:output] = File.open(o, 'w')
+  end
+end
+
+optparse.parse!
+
+unless options[:file]
+  puts 'Missing option: file'
+  puts optparse
+  exit
+end
 
 ObjectSpace.each_object(Class).select { |c| c < Activity }.each do |c|
   ScenarioBuilder.register_activity_class(c)
 end
 scenario_builder = ScenarioBuilder.new
-scenario_builder.instance_eval(File.read(SCENARIO_PATH), SCENARIO_PATH)
+scenario_builder.instance_eval(File.read(options[:file]), options[:file])
 scenario = scenario_builder.activities
 logger = Log.new(
-  out: $stdout,
-  context: Context.resolve,
-  formatter: LogFormat.for_name(:pretty)
+  out: options[:output],
+  context: context,
+  formatter: LogFormat.for_name(options[:log_format])
 )
 scenario_runner = ScenarioRunner.new(log: logger)
 scenario_runner.run(scenario)
