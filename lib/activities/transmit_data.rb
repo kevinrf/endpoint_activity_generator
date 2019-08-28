fd36e99d8415917289ac78808a476faac5c3a13f
Kevin Fuhst <kfuhst@gmail.com>
2019-08-28 15:24:57 -0600

    log local and remote address/port of socket transmissions

--- lib/activities/transmit_data.rb
+++ lib/activities/transmit_data.rb
@@ -9,11 +9,19 @@ class TransmitData < Activity
 
   def perform
     TCPSocket.open(@host, @port) do |socket|
+      _, @local_port, @local_ip = socket.addr
+      _, @remote_port, @remote_ip = socket.peeraddr
       @bytes_sent = socket.write(@data)
     end
   end
 
   def log_data
-    {bytes_sent: @bytes_sent || 0}
+    {
+      remote_ip: @remote_ip,
+      remote_port: @remote_port,
+      local_ip: @local_ip,
+      local_port: @local_port,
+      bytes_sent: @bytes_sent || 0,
+    }
   end
 end
